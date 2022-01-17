c1db33bc4352008cf81a8df59f95336d7ae0e9c0
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-17 19:05:26 +0530

    pushing new project

---
+++ ControllersDemo/ControllersDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return Content("Welcome to employee page");
+        }
+
+        [OutputCache(Duration =5)]
+        public ActionResult GetTime()
+        {
+            return Content(DateTime.Now.ToString());
+        }
+
+        [ActionName("GetByCode")]
+
+        public ActionResult GetEmployeeDataByEmpCode()
+        {
+            return Content("Emp Code is 100");
+        }
+
+        public ActionResult SearchEmp()
+        {
+            return RedirectToAction("GetByCode");
+        }
+
+        [NonAction]
+        public string Test()
+        {
+            return "This is Test";
+        }
+
+        public ActionResult CallTest()
+        {
+            return RedirectToAction("Test");
+        }
+
+        [HttpGet]
+        public ActionResult LoginView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return Content("User Logged In");
+        }
+
+    }
+}
---
+++ ControllersDemo/ControllersDemo/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Welcome to Index Page";
+        }
+
+        public string  AboutUs()
+        {
+            return "Welcome to About-US";
+        }
+
+        public string GetEmpCode(int id)
+        {
+            return $"Employee Code:{id}";
+        }
+
+        public ActionResult IndexView()
+        {
+            return View();
+            //return View("ViewName");
+        }
+
+        public ActionResult NavigateToMsdn()
+        {
+            return Redirect("https://www.msdn.com");
+        }
+
+        public ActionResult NavigateToIndex()
+        {
+            return RedirectToAction("IndexView");
+            //  if action method is in different controller
+            //return Redirect("Controller/ActionName");
+        }
+
+        public ActionResult RedirectToNewRoute()
+        {
+            return RedirectToRoute(new { 
+                controller="Employee",
+                action="Index"
+            }); ;
+        }
+
+        public ActionResult PrintMessage()
+        {
+            return Content("<b>This is Test Message</b>");
+        }
+
+        public ActionResult OpenFile()
+        {
+            return new FilePathResult(@"~\Files\angularjs_tutorial.pdf", "application/pdf");
+        }
+
+        public ActionResult ShowEmpty()
+        {
+            return new EmptyResult();
+        }
+    }
+}
